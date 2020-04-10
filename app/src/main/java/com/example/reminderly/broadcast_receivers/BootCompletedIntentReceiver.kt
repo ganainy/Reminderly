1661bd1072bbc817c85249e14d5ccc905334846c
amr <afoda50@gmail.com>
2020-04-11 00:26:20 +0200

    reschedule alarms after device is rebooted

---
+++ app/src/main/java/com/example/reminderly/broadcast_receivers/BootCompletedIntentReceiver.kt
@@ -0,0 +1,38 @@
+package com.example.reminderly.broadcast_receivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.example.footy.database.ReminderDatabase
+import com.example.reminderly.Utils.MyUtils
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import java.util.*
+
+/**reschedule alarms after device is rebooted*/
+class BootCompletedIntentReceiver : BroadcastReceiver() {
+
+    var compositeDisposable=CompositeDisposable()
+    override fun onReceive(context: Context, intent: Intent) {
+
+
+        if ("android.intent.action.BOOT_COMPLETED" == intent.action) {
+          restartUpcomingAlarms(context)
+        }
+    }
+
+    private fun restartUpcomingAlarms(context: Context) {
+        val reminderDatabaseDao = ReminderDatabase.getInstance(context).reminderDatabaseDao
+        compositeDisposable.add (reminderDatabaseDao.getUpcomingReminders(Calendar.getInstance().timeInMillis).subscribeOn(
+            Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                for (reminder in it){
+                    MyUtils.addAlarmManager(reminder.id,context,reminder.createdAt.timeInMillis,reminder.repeat)
+                }
+                compositeDisposable.clear()
+            })
+    }
+}
